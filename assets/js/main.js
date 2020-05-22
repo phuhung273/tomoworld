7ac3089fd7b3817c10c4418fa42bf8f263fdb7c6
phuhung273 <phuhung.tranpham@gmail.com>
2020-05-22 09:20:34 +0700

    scroll top button + feedback 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
-  var navbar_height = $(".navbar").outerHeight();
-  $(".navbar-spacer").css("padding-top", navbar_height);
+  var header_height = $(".header").outerHeight();
+  $(".header-spacer").css("padding-top", header_height);
 
   AOS.init();
+
+  scroll_top_button = $(".btn-scroll-top");
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scroll_top_button.css("display", "block");
+    } else {
+      scroll_top_button.css("display", "none");
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
 });
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
